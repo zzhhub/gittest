60b5b2d4dd0c582425c726e3eb2aa17c67ffe720
zzhhub <864366334@qq.com>
2020-12-18 16:59:18 +0800

    webpack处理多个 entry output

--- bTest/webpack.config.js
+++ bTest/webpack.config.js
@@ -2,9 +2,17 @@ const path = require('path');
 
 module.exports = {
     mode: "development",
-    entry: './src/index.js',
+    // entry: "./src/index.js",
+    entry: {
+        main: './src/index.js',
+        search: './src/search.js',
+    },
+    // output: {
+    //     path: path.resolve(__dirname, 'dist'),
+    //     filename: 'bundle.js'
+    // }
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: '[name].js'
     }
 };
